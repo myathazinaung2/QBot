cb0f740801677c93e92bd462a0d5464dcf5ec6dc
Mya <72430971+myathazinaung2@users.noreply.github.com>
2020-10-06 12:10:05 +0630

    Update environment.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://qbotestalfa.azurewebsites.net/api/Request/",
+    selfUrl: "https://qbotestalfa-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "aa81ea33-cf73-444b-b305-006b188a8fb4",
+        clientId: "7be31db2-3861-4ff1-8038-7fcbf7eea125",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
